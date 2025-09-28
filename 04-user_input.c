a139d448b607fa9de18b942fb54852873db20df6
Your Name <youremail@example.com>
2025-09-28 20:43:39 +0300

    Update 04-user_input.c with user input examples

--- 04-user_input.c
+++ 04-user_input.c
@@ -2,7 +2,28 @@
  
  int main(){
     
-    int age;
-    
+   //Example 1: Reading and printing an integer
 
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("%d\n", age);
+
+   //Example 2: Reading and printing a double and Character
+   double number;
+   char alphabet;
+
+   printf("Enter a double value: ");   
+   scanf("%lf", &number);
+
+   printf("Enter a character: ");
+   scanf("\n%c", &alphabet); 
+
+   printf("You entered: %.2lf and %c\n", number, alphabet);
+
+   
+   return 0;
+
+    
  }
