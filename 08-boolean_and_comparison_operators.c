a7c262ded4cf6f161b381d9a924edbd6f30d8380
Your Name <youremail@example.com>
2025-09-28 21:32:48 +0300

    Add 08-boolean_and_comparison_operators.c for boolean and comparison operators examples

---
+++ 08-boolean_and_comparison_operators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    int a = 5, b = 10;
+
+    // Comparison Operators
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n", a != b);
+    printf("a > b: %d\n", a > b);
+    printf("a < b: %d\n", a < b);
+    printf("a >= b: %d\n", a >= b);
+    printf("a <= b: %d\n", a <= b);
+
+    // Boolean Variables
+    bool isTrue = true;
+    bool isFalse = false;
+    printf("isTrue: %d\n", isTrue);
+    printf("isFalse: %d\n", isFalse);
+
+    return 0;
+}
